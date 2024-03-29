8b6e22e915e3e2f0f8c21f600b281cbb06f67f7f
Tormod <tormod.flesjo@gmail.com>
2024-03-29 13:54:44 +0100

    quick commit

--- src/app/oslo-byrad-vedtar-utvidet-skjenketid/components/Nav.tsx
+++ src/app/oslo-byrad-vedtar-utvidet-skjenketid/components/Nav.tsx
@@ -6,7 +6,7 @@ export default function Nav() {
   return (
     <nav className='flex justify-center bg-agrey h-[42px] mb-14'>
       <div className='w-[1020px] flex items-center justify-between border-2 '>
-        <ul className='flex h-full'>
+        <ul className='hidden md:flex h-full'>
           <li className='hover:bg-gray-100'>
             <Link
               className='px-3 h-full w-full flex items-center justify-center'
